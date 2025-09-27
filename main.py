8cdb4eda082a497c482110f242728331d14baa21
SquidDrilll <bhavyakapatel2008@gmail.com>
2025-09-27 21:49:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 from selfbot import SelfBot
 from tldr import setup_tldr
+from chatbot import setup_chat 
 
 load_dotenv()
 
@@ -13,6 +14,7 @@ bot = SelfBot(
 )
 
 setup_tldr(bot)
+setup_chat(bot)
 
 if __name__ == "__main__":
     bot.run()
